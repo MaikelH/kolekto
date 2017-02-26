aafbce47919eff19662cd1504aeb1d4d1bf3b4de
M. Hofman <m.hofman@chronos-technology.nl>
2017-02-27 00:00:38 +0100

    [Bloomfilter] Add calculations for optimal number of bits and hash functions

--- lib/BloomFilter.ts
+++ lib/BloomFilter.ts
@@ -3,14 +3,16 @@
 export class BloomFilter<T> {
 	private _fpp: number;
 
-	constructor(expectedInsertions: number);
-
-	constructor(private expectedInsertions: number, fpp?: number) {
+	constructor(expectedInsertions: number, fpp?: number) {
 		if(fpp == undefined) {
 			fpp = 0.03;
 
 			this.initialize(expectedInsertions, fpp);
 		}
+		else {
+			this.initialize(expectedInsertions, fpp);
+		}
+
 	}
 
 	public expectedFpp() : number {
@@ -34,7 +36,10 @@ export class BloomFilter<T> {
 	}
 
 	private initialize(expectedInsertions: number, fpp: number) {
+		let numberOfBits = this.calculateNumberOfBits(expectedInsertions, fpp);
+		let hashFunctions = this.calculateOptimalNumberHashFunctions(numberOfBits, expectedInsertions);
 
+		console.log(numberOfBits, hashFunctions);
 	}
 
 	/**
@@ -48,5 +53,19 @@ export class BloomFilter<T> {
 	 */
 	private calculateErrorRate(expectedInsertions: number, numberOfBits: number, numberOfHashes: number) {
 
+	}
+
+	private calculateNumberOfBits(expectedInsertions: number, fpp: number) : number {
+		return Math.ceil((-1* expectedInsertions * Math.log(fpp)) / Math.pow(Math.log(2), 2));
+	}
+
+	/**
+	 * Calculate optimal number of hash functions.
+	 *
+	 * @param numberOfBits
+	 * @param expectedInsertions
+	 */
+	private calculateOptimalNumberHashFunctions(numberOfBits: number, expectedInsertions: number) : number {
+		return Math.ceil((numberOfBits / expectedInsertions) * Math.log(2));
 	}
 }
--- test/BloomFilter-test.ts
+++ test/BloomFilter-test.ts
@@ -7,7 +7,7 @@ import {BloomFilter} from "../lib/BloomFilter";
 describe("BloomFilter", () => {
 	describe('Construction', () => {
 		it('Should create bloomfilter with fpp of 0.03 when initialized', () => {
-			let filter = new BloomFilter(10000);
+			let filter = new BloomFilter(216553,0.01);
 
 			//Assert.equal(filter.expectedFpp(), 0.03);
 		})
