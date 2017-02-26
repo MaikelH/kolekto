2af26298bfbe359ee391bda254fe7db199dfb7a2
M. Hofman <m.hofman@chronos-technology.nl>
2017-02-27 00:21:58 +0100

    [Bloomfilter] Implement expected false positive probability function

--- lib/BloomFilter.ts
+++ lib/BloomFilter.ts
@@ -1,9 +1,15 @@
 
 
+import {BitArray} from "./collections/BitArray";
 export class BloomFilter<T> {
 	private _fpp: number;
+	private data: BitArray;
+
+	private expectedInsertions;
 
 	constructor(expectedInsertions: number, fpp?: number) {
+		this.expectedInsertions = expectedInsertions;
+
 		if(fpp == undefined) {
 			fpp = 0.03;
 
@@ -16,7 +22,7 @@ export class BloomFilter<T> {
 	}
 
 	public expectedFpp() : number {
-		return 0;
+		return this.calculateErrorRate(this.expectedInsertions, this.data.getSize());
 	}
 
 	public isCompatible(that: BloomFilter<T>) {
@@ -39,7 +45,7 @@ export class BloomFilter<T> {
 		let numberOfBits = this.calculateNumberOfBits(expectedInsertions, fpp);
 		let hashFunctions = this.calculateOptimalNumberHashFunctions(numberOfBits, expectedInsertions);
 
-		console.log(numberOfBits, hashFunctions);
+		this.data = new BitArray(numberOfBits);
 	}
 
 	/**
@@ -47,20 +53,37 @@ export class BloomFilter<T> {
 	 *
 	 * See for more information: https://en.wikipedia.org/wiki/Bloom_filter#Probability_of_false_positives
 	 *
+	 * p = e^(-(m / n) * (ln(2)^2))
+	 *
 	 * @param expectedInsertions
 	 * @param numberOfBits
 	 * @param numberOfHashes
 	 */
-	private calculateErrorRate(expectedInsertions: number, numberOfBits: number, numberOfHashes: number) {
-
+	private calculateErrorRate(expectedInsertions: number, numberOfBits: number) {
+		return Math.pow(Math.E, (-(numberOfBits / expectedInsertions) * Math.pow(Math.log(2), 2)));
 	}
 
+	/**
+	 * Calculate the optimal number of bits
+	 *
+	 * m = -n*ln(p) / (ln(2)^2)
+	 *
+	 * where m is the number of bits.
+	 *
+	 * @param expectedInsertions
+	 * @param fpp
+	 * @return {number}
+	 */
 	private calculateNumberOfBits(expectedInsertions: number, fpp: number) : number {
 		return Math.ceil((-1* expectedInsertions * Math.log(fpp)) / Math.pow(Math.log(2), 2));
 	}
 
 	/**
 	 * Calculate optimal number of hash functions.
+	 *
+	 * k = m/n * ln(2)
+	 *
+	 * where k is the number of hash functions.
 	 *
 	 * @param numberOfBits
 	 * @param expectedInsertions
--- test/BloomFilter-test.ts
+++ test/BloomFilter-test.ts
@@ -1,15 +1,20 @@
-import * as sinon from "sinon";
 import {assert as Assert} from "chai";
 import {BloomFilter} from "../lib/BloomFilter";
-
-
 
 describe("BloomFilter", () => {
 	describe('Construction', () => {
 		it('Should create bloomfilter with fpp of 0.03 when initialized', () => {
-			let filter = new BloomFilter(216553,0.01);
+			let filter = new BloomFilter<Number>(10000);
 
-			//Assert.equal(filter.expectedFpp(), 0.03);
+			Assert.isTrue(isEqualFloat(filter.expectedFpp(), 0.03, 0.0001))
 		})
 	});
+
+	function isEqualFloat(actual: number, expected: number, epsilon: number) : boolean{
+		if(actual <= expected + epsilon && actual >= expected - epsilon) {
+			return true;
+		}
+
+		return false;
+	}
 });
